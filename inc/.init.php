7db0ee81e5d4fa6c658fccc69f2215a11dbd70e5
pebriniel <zeldalyoko@gmail.com>
2017-01-03 20:01:43 +0100

    modify .init.php
    change content of .init.php

--- inc/.init.php
+++ inc/.init.php
@@ -1,6 +1,5 @@
 <?php
 
-if($_SERVER['REMOTE_ADDR'] == "127.0.0.1"){
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', '');
@@ -10,17 +9,5 @@ if($_SERVER['REMOTE_ADDR'] == "127.0.0.1"){
 	define('FOLD_EMAIL', 'template/email/');
 	define('PREFIX_DB', 'POP_');
 	define('TPL_EMP', 'template/themeA');
-}
-else{
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'boussads');
-    define('DB_PASS', 'JqsWM87PwX');
-    define('DB_DATA', 'boussads');
-	
-	define('URL_', 'http://boussads.student.codeur.online/share/');
-	define('FOLD_EMAIL', 'template/email/');
-	define('PREFIX_DB', 'POP_');
-	define('TPL_EMP', 'template/themeA');
-}
 
 	define('EXT_MU_TPL', '.html');
